1d9dd9e7378deb7988faea979c7740f505575583
Lakaganth <lakaganth89@gmail.com>
2019-06-20 16:08:30 +0530

    Final submit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const renderResult = parks => {
 <div class="card mt-2 " >  
   <div class="card-body">
     <h5 class="card-title">${park.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <h6 class="card-subtitle mb-2 text-muted">${park.designation}</h6>
     <p class="card-text">${park.description}</p>
     <a  target="_blank" href="${
       park.url
